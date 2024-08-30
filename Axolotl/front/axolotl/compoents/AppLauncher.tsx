bbfa352c4e8f16f6d7fbef56cfc33e5823b81054
Your Name <john.farmer1086@gmail.com>
2024-08-30 13:20:17 +0800

    created componets Chat interface and launcher interface

---
+++ Axolotl/front/axolotl/compoents/AppLauncher.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const AppLauncher: React.FC = () => {
+  return (
+    <div>
+      <h1>App Launcher</h1>
+      {/* Implement app launcher UI */}
+    </div>
+  );
+};
+
+export default AppLauncher;
---
+++ Axolotl/front/axolotl/compoents/Chat.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Chat: React.FC = () => {
+  return (
+    <div>
+      <h1>Chat Interface</h1>
+      {/* Implement chat UI */}
+    </div>
+  );
+};
+
+export default Chat;
