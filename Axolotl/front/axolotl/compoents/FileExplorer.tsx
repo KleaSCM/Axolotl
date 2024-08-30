ff3a5e82a61a67d2eb55b75695682c03c22921e1
Your Name <john.farmer1086@gmail.com>
2024-08-30 13:22:02 +0800

    created file explorer component

---
+++ Axolotl/front/axolotl/compoents/FileExplorer.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const FileExplorer: React.FC = () => {
+  return (
+    <div>
+      <h1>File Explorer</h1>
+      {/* Implement file explorer UI */}
+    </div>
+  );
+};
+
+export default FileExplorer;
