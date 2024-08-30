e74e0e360dc7cb65fc8c0a848726dc320c866774
Your Name <john.farmer1086@gmail.com>
2024-08-30 13:23:26 +0800

    created basic SpotifyControl component

---
+++ Axolotl/front/axolotl/compoents/SpotifyControl.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const SpotifyControl: React.FC = () => {
+  return (
+    <div>
+      <h1>Spotify Control</h1>
+      {/* Implement Spotify control UI */}
+    </div>
+  );
+};
+
+export default SpotifyControl;
