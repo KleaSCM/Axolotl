465071dc26d0e8b11d94e9fe19969ccd1b78dab4
Your Name <john.farmer1086@gmail.com>
2024-09-04 09:42:44 +0800

    handle for accsess tokens

--- Axolotl/front/axolotl/components/Spotify.tsx
+++ Axolotl/front/axolotl/components/Spotify.tsx
@@ -1,11 +1,50 @@
+//add spotify redirect for user accsess token 
+// import React from 'react';
+// import styles from './Spotify.module.scss';
 
-import React from 'react';
+// const CLIENT_ID = '3480f05891fc44d5ad2b161451a47979'; 
+// const REDIRECT_URI = 'http://localhost:3000'; 
+
+// const Spotify: React.FC = () => {
+//   const handleLogin = () => {
+//     const scopes = 'user-library-read user-read-private';
+//     const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
+//     window.location.href = authUrl;
+//   };
+
+//   return (
+//     <div className={styles['spotify-container']}>
+//       <button onClick={handleLogin}>Login with Spotify</button>
+//     </div>
+//   );
+// };
+
+// export default Spotify;
+
+
+
+
+//handle access token 
+import React, { useEffect, useState } from 'react';
 import styles from './Spotify.module.scss';
 
 const CLIENT_ID = '3480f05891fc44d5ad2b161451a47979'; 
 const REDIRECT_URI = 'http://localhost:3000'; 
 
 const Spotify: React.FC = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Get access token from URL hash
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.substring(1));
+    const accessToken = params.get('access_token');
+    if (accessToken) {
+      setToken(accessToken);
+      window.history.replaceState({}, document.title, '/'); // Clean up URL
+    }
+  }, []);
+
   const handleLogin = () => {
     const scopes = 'user-library-read user-read-private';
     const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
@@ -14,7 +53,14 @@ const Spotify: React.FC = () => {
 
   return (
     <div className={styles['spotify-container']}>
-      <button onClick={handleLogin}>Login with Spotify</button>
+      {!token ? (
+        <button onClick={handleLogin}>Login with Spotify</button>
+      ) : (
+        <div>
+          <p>Logged in with Spotify</p>
+          {/* make API calls using the access token */}
+        </div>
+      )}
     </div>
   );
 };
