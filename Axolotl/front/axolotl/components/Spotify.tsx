8795f8a289abfbc62a719f57932b11a8ebe1128a
Your Name <john.farmer1086@gmail.com>
2024-09-04 09:40:08 +0800

    added spotify redirect

--- Axolotl/front/axolotl/components/Spotify.tsx
+++ Axolotl/front/axolotl/components/Spotify.tsx
@@ -1,11 +1,20 @@
+
 import React from 'react';
-import styles from '.\./styles/Spotify.module.scss'; 
+import styles from './Spotify.module.scss';
+
+const CLIENT_ID = '3480f05891fc44d5ad2b161451a47979'; 
+const REDIRECT_URI = 'http://localhost:3000'; 
 
 const Spotify: React.FC = () => {
+  const handleLogin = () => {
+    const scopes = 'user-library-read user-read-private';
+    const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
+    window.location.href = authUrl;
+  };
+
   return (
     <div className={styles['spotify-container']}>
-      <div>Spotify Section</div>
-      {/* Add additional Spotify-related content here */}
+      <button onClick={handleLogin}>Login with Spotify</button>
     </div>
   );
 };
